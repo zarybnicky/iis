e2412a38e6ac6d55384187b2ce92555f43388cca
Jakub Zárybnický <jakub.zarybnicky@inuits.eu>
2017-11-29 10:47:13 +0100

    .dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((haskell-mode . ((haskell-indent-spaces . 4)
+                  (haskell-process-use-ghci . t)
+                  (intero-project-root . "/home/inuits/iis")))
+ (hamlet-mode . ((hamlet/basic-offset . 4)
+                 (haskell-process-use-ghci . t))))
